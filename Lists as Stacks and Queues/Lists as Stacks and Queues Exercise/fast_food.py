ff302e501888dfd66de10ebd9df28a5937e105ac
Petar Ivanov <petarivanov95@gmail.com>
2023-02-04 14:08:03 +0200

    problems

--- Lists as Stacks and Queues/Lists as Stacks and Queues Exercise/fast_food.py
+++ Lists as Stacks and Queues/Lists as Stacks and Queues Exercise/fast_food.py
@@ -10,7 +10,7 @@ local_orders = orders.copy() # create a copy for the loop, which will not be aff
 
 for order in local_orders:
     if food_qty - order >= 0: # checks for food availability
-        orders.popleft() # removes FIFO order
+        orders.popleft() # removes item 
         food_qty -= order # subtracts the particular order from the total food qty we have
     else:
         print(f"Orders left: {' '.join([str(o) for o in orders])}")
