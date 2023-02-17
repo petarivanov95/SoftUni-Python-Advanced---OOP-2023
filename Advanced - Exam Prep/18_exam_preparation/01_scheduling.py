b0d0c26d3b60cfb3497a0aa36571e1dcaffb890c
Petar Ivanov <petarivanov95@gmail.com>
2023-02-17 21:52:38 +0200

    Further explanation for 01_scheduling

--- Advanced - Exam Prep/18_exam_preparation/01_scheduling.py
+++ Advanced - Exam Prep/18_exam_preparation/01_scheduling.py
@@ -5,9 +5,15 @@ selected_index = int(input())
 
 def sjf(jobs, index):
     # Sort the list of jobs by their clock-cycles in ascending order,
-    # with ties broken by their original order in the list (FIFO)
-    sorted_jobs = sorted(enumerate(jobs), key=lambda x: (x[1], x[0]))
+    # with those with equal result broken by their original order in the list (FIFO)
+    # ---
+    # enumerate takes the original index and job, this will be used to then sort them
+    # by x[1], which is the job size and if the sizes are the same at x[1] then x[0]
+    # will be used, which in turn will rank them based on the index from the original list
+    # to adhere to the FIFO order requirement
     
+    sorted_jobs = sorted(enumerate(jobs), key=lambda x: (x[1], x[0]))
+
     # Start with a time of 0
     time = 0
     
